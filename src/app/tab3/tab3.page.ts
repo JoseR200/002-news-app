4979aa98d15cc1f1fbb9c95d4a4a36f976ef5178
JoseR200 <85463913+JoseR200@users.noreply.github.com>
2024-11-24 13:47:48 +0100

    Updste to open browser

--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -1,12 +1,16 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Browser} from "@capacitor/browser";
 
 @Component({
   selector: 'app-tab3',
   templateUrl: 'tab3.page.html',
   styleUrls: ['tab3.page.scss']
 })
-export class Tab3Page {
+export class Tab3Page implements OnInit {
 
   constructor() {}
 
+  async ngOnInit() {
+    await Browser.open({'url': 'https://capacitor.ionicframework.com/'});
+  }
 }
